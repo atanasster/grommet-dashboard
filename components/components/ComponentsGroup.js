82c9b194f11c16884f3c61b4ba5e188e93b125c2
atanasster <atanasster@gmail.com>
2018-10-12 11:41:09 +0300

    filter components, add truncated description

--- components/components/ComponentsGroup.js
+++ components/components/ComponentsGroup.js
@@ -6,47 +6,66 @@ import RoutedButton from '../RoutedButton';
 import Section from './Section';
 import Example from './Example';
 
-export default ({ examples, group }) => (
-  <Section name={group}>
-    {Object.keys(examples).filter(key => (examples[key].category === group)).sort().map(item => (
-      <Card background='brand' key={`${group}_${item}`} basis='medium' margin='xsmall'>
-        <Card.CardTitle justify='between'>
-          <RoutedButton route='documentation' params={{ component: item }}>
-            <Heading margin='none' level={3}>
-              {item}
-            </Heading>
-          </RoutedButton>
-          <Text size='small'>
-            {examples[item].package}
-          </Text>
-        </Card.CardTitle>
-        <Card.CardContent flex={false} basis='220px' align='center' justify='center'>
-          <Example code={examples[item].examples._starter} />
-        </Card.CardContent>
-        <Card.CardActions>
-          <Box direction='row' justify='between' fill='horizontal'>
-            <RoutedButton
-              route='documentation'
-              params={{ component: item }}
-            >
-              <Box direction='row' gap='xsmall' pad='xsmall'>
-                <Document />
-                Docs
+export default ({ examples, group, search }) => {
+  const filtered = Object.keys(examples).filter((key) => {
+    if (search && search.length > 0 && !key.toLowerCase().match(search.toLowerCase())) {
+      return false;
+    }
+    return examples[key].category === group;
+  });
+  if (filtered.length === 0) {
+    return null;
+  }
+  return (
+    <Section name={group}>
+      {filtered.sort().map(item => (
+        <Card background='brand' key={`${group}_${item}`} basis='medium' margin='xsmall'>
+          <Card.CardTitle justify='between'>
+            <Box gap='small' fill='horizontal'>
+              <Box direction='row' justify='between'>
+                <RoutedButton route='documentation' params={{ component: item }}>
+                  <Heading margin='none' level={3}>
+                    {item}
+                  </Heading>
+                </RoutedButton>
+                <Text size='small'>
+                  {examples[item].package}
+                </Text>
               </Box>
-            </RoutedButton>
+              <Text size='small' truncate={true}>
+                {examples[item].doc && examples[item].doc.description}
+              </Text>
+            </Box>
 
-            <RoutedButton
-              route='examples'
-              params={{ group: item, example: '_starter' }}
-            >
-              <Box direction='row' gap='xsmall' pad='xsmall'>
-                <Code />
-                Code
-              </Box>
-            </RoutedButton>
-          </Box>
-        </Card.CardActions>
-      </Card>
+          </Card.CardTitle>
+          <Card.CardContent flex={false} basis='220px' align='center' justify='center'>
+            <Example code={examples[item].examples._starter} />
+          </Card.CardContent>
+          <Card.CardActions>
+            <Box direction='row' justify='between' fill='horizontal'>
+              <RoutedButton
+                route='documentation'
+                params={{ component: item }}
+              >
+                <Box direction='row' gap='xsmall' pad='xsmall'>
+                  <Document />
+                  Docs
+                </Box>
+              </RoutedButton>
+
+              <RoutedButton
+                route='examples'
+                params={{ group: item, example: '_starter' }}
+              >
+                <Box direction='row' gap='xsmall' pad='xsmall'>
+                  <Code />
+                  Code
+                </Box>
+              </RoutedButton>
+            </Box>
+          </Card.CardActions>
+        </Card>
       ))}
-  </Section>
-);
+    </Section>
+  );
+};
--- pages/components.js
+++ pages/components.js
@@ -1,10 +1,15 @@
 import 'isomorphic-fetch';
 import { Box } from 'grommet';
+import { DropInput } from 'grommet-controls';
+import { Search } from 'grommet-icons';
 import SiteLayout from '../components/layouts/SiteLayout';
+import Title from '../components/layouts/Title';
 import ComponentsGroup from '../components/components/ComponentsGroup';
 
 
 export default class AddOns extends React.Component {
+  state = { search: '' };
+
   static async getInitialProps() {
     const res = await fetch('https://grommet-nextjs.herokuapp.com/api/examples');
     const allExamples = await res.json();
@@ -13,15 +18,32 @@ export default class AddOns extends React.Component {
 
   render() {
     const { examples } = this.props;
+    const { search } = this.state;
     return (
       <SiteLayout title='components'>
+        <Title label='Components'>
+          <Box direction='row' basis='medium'>
+            <DropInput
+              style={{
+                WebkitAppearance: 'none',
+              }}
+
+              value={search}
+              type='search'
+              onChange={({ target: { value } }) => this.setState({ search: value })}
+              widgets={[
+                { icon: <Search color='brand' />, onClick: () => {} },
+              ]}
+            />
+          </Box>
+        </Title>
         <Box>
-          <ComponentsGroup examples={examples} group='Layout' />
-          <ComponentsGroup examples={examples} group='Type' />
-          <ComponentsGroup examples={examples} group='Controls' />
-          <ComponentsGroup examples={examples} group='Input' />
-          <ComponentsGroup examples={examples} group='Visualizations' />
-          <ComponentsGroup examples={examples} group='Media' />
+          <ComponentsGroup examples={examples} search={search} group='Layout' />
+          <ComponentsGroup examples={examples} search={search} group='Type' />
+          <ComponentsGroup examples={examples} search={search} group='Controls' />
+          <ComponentsGroup examples={examples} search={search} group='Input' />
+          <ComponentsGroup examples={examples} search={search} group='Visualizations' />
+          <ComponentsGroup examples={examples} search={search} group='Media' />
         </Box>
       </SiteLayout>
     );
